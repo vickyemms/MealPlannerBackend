4778422eb8d332d5c9dd213f18449f8cde71b873
vickyemms <64374691+vickyemms@users.noreply.github.com>
2025-01-26 19:30:46 +0100

    fix CORS issues

--- src/main/java/com/vegetarianmealplanner/meal_planner/controller/RecipeController.java
+++ src/main/java/com/vegetarianmealplanner/meal_planner/controller/RecipeController.java
@@ -5,12 +5,14 @@ import com.vegetarianmealplanner.meal_planner.repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor // Lombok annotation for constructor injection
 public class RecipeController {
